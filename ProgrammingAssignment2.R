c593297f48c2caba37270058a2e6874598eff8b7
RocketShawn <Shawn.Tang.7@gmail.com>
2016-07-14 13:53:32 -0500

    Update ProgrammingAssignment2.R

--- ProgrammingAssignment2.R
+++ ProgrammingAssignment2.R
@@ -1,3 +1,6 @@
+## In this first step, we must create a cached Matrix
+## by setting the starting values of the matrix, as well as the inverse of that matrix
+
 makeCacheMatrix <- function(x = matrix()) {
 ma <- NULL
 set <- function(y) {
@@ -16,6 +19,9 @@ ginverse = ginverse
 }
 
 
+## The purporse of the next function is to calculate the inverse of a matrix
+## as defined by the makeCacheMatrix function
+
 cacheSolve <- function(x, ...) {
 ma <- x$ginverse()
 if(!is.null(ma)) {
@@ -30,7 +36,7 @@ ma
 
 
 
-
+## These are the commands to test that the inverse matrix is being calculated correctly
 
 myMatrix=makeCacheMatrix(matrix(1:4, nrow=2, ncol=2, byrow=FALSE))
 cacheSolve(myMatrix)
